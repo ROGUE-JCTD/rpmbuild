069b61d24d34da3d4e272aa0391fa39f4c618fcc
Daniel Berry <dberry@boundlessgeo.com>
2015-12-17 11:23:14 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
-yum -y install http://yum.geoshape.org/geoshape-repo-1.0.0-1.el6.noarch.rpm
+pushd /etc/yum.repos.d
+wget http://yum.geoshape.org/GeoSHAPE-rpmbuild.repo
+popd
 sudo yum -y update
-sudo yum -y install gcc gcc-c++ make expat-devel db4-devel gdbm-devel sqlite-devel readline-devel zlib-devel bzip2-devel openssl-devel tk-devel gdal-devel-1.11.2 libxslt-devel libxml2-devel libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel littlecms-devel proj-devel geos-devel postgresql93-devel unzip rpmdevtools git python27-devel python27-virtualenv
+sudo yum -y install gcc gcc-c++ make expat-devel db4-devel gdbm-devel sqlite-devel readline-devel zlib-devel bzip2-devel openssl-devel tk-devel gdal-devel-1.11.2 libxslt-devel libxml2-devel libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel littlecms-devel proj-devel geos-devel postgresql93-devel unzip rpmdevtools git python27-devel python27-virtualenv createrepo
 pushd /vagrant/SOURCES
 ./get_sources.sh
 popd
