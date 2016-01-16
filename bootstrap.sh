b7122fb7b41deb8b003144dff9dbf73a53738fc0
Daniel Berry <dberry@boundlessgeo.com>
2016-01-16 13:17:34 -0600

    update java for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ pushd /etc/yum.repos.d
 wget http://yum.geoshape.org/GeoSHAPE-rpmbuild.repo
 popd
 sudo yum -y update
-sudo yum -y install gcc gcc-c++ make expat-devel db4-devel gdbm-devel sqlite-devel readline-devel zlib-devel bzip2-devel openssl-devel tk-devel libxslt-devel libxml2-devel libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel littlecms-devel proj-devel geos-devel postgresql93-devel zip unzip rpmdevtools git python27-devel python27-virtualenv createrepo curl-devel poppler-devel xerces-c-devel java-1.6.0-openjdk-devel ant chrpath libtool swig cmake doxygen libpng-devel
+sudo yum -y install gcc gcc-c++ make expat-devel db4-devel gdbm-devel sqlite-devel readline-devel zlib-devel bzip2-devel openssl-devel tk-devel libxslt-devel libxml2-devel libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel littlecms-devel proj-devel geos-devel postgresql93-devel zip unzip rpmdevtools git python27-devel python27-virtualenv createrepo curl-devel poppler-devel xerces-c-devel java-1.8.0-openjdk-devel ant chrpath libtool swig cmake doxygen libpng-devel
 pushd /vagrant/SOURCES
 ./get_sources.sh
 popd
