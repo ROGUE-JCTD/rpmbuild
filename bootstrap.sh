d78665227921bd42e86b950e92f98b9f55722208
Daniel Berry <dberry@boundlessgeo.com>
2016-01-19 16:52:42 -0600

    Added erlang.spec

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@ pushd /etc/yum.repos.d
 wget http://yum.geoshape.org/GeoSHAPE-rpmbuild.repo
 popd
 sudo yum -y update
-sudo yum -y install gcc gcc-c++ make expat-devel db4-devel gdbm-devel sqlite-devel readline-devel zlib-devel bzip2-devel openssl-devel tk-devel libxslt-devel libxml2-devel libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel littlecms-devel proj-devel geos-devel postgresql93-devel zip unzip rpmdevtools git python27-devel python27-virtualenv createrepo libcurl-devel poppler-devel xerces-c-devel java-1.8.0-openjdk-devel ant chrpath libtool swig cmake doxygen libpng-devel cppunit autoconf libgcj-devel 
+sudo yum -y install gcc gcc-c++ make expat-devel db4-devel gdbm-devel sqlite-devel readline-devel zlib-devel bzip2-devel openssl-devel tk-devel libxslt-devel libxml2-devel libjpeg-turbo-devel zlib-devel libtiff-devel freetype-devel littlecms-devel geos-devel postgresql95-devel zip unzip rpmdevtools git python27-devel python27-virtualenv createrepo libcurl-devel poppler-devel xerces-c-devel java-1.8.0-openjdk-devel ant chrpath libtool swig cmake doxygen libpng-devel cppunit autoconf libgcj-devel
 pushd /vagrant/SOURCES
 ./get_sources.sh
+
 popd
